be8534c200dc466cc49d18d55b6a33f8f4476160
Jiwon <awesomedev3@rockwonitglobal.com>
2024-10-20 11:09:47 +0900

    fix: TitleBox 컴포넌트 글씨체 수정

--- src/components/TitleBox.vue
+++ src/components/TitleBox.vue
@@ -2,7 +2,7 @@
     <div v-if="description" style="display: flex; background-color: white; position: relative; top: 10px;"
         :style="`width: ${width}; height: 3rem;`" class="flex justify-between border-solid border-2 
         border-gray-200 py-3 px-[25px] cursor-pointer rounded-[16px] hover:bg-gray-200">
-        <span class="text-[1.1rem] font-bold">
+        <span class="text-[1.1rem] font-black">
             {{ title }}
         </span>
         <span class="text-[0.9rem]">
