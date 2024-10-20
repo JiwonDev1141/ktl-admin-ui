31bcca7ceb38260db340b049f4ef1e6a7b8cba5c
Jiwon <awesomedev3@rockwonitglobal.com>
2024-10-20 11:11:13 +0900

    feat: 일반 사용자 장비 예약 화면 기본 퍼블리싱

---
+++ src/pages/User/Reservation/ImageCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="rounded-md min-w-[260px] border-[2px] border-grey-200 flex flex-col items-center cursor-pointer mx-2"
+        :class="isSelected ? 'border-[5px] border-primaryColor' : 'border-[2px] border-grey-200'">
+        <span class="rounded-md my-1 w-[90%] text-center font-bold" :style="`border: solid 1.5px ${color}`">{{ title
+            }}</span>
+        <img class="w-full " :src="imageSrc" />
+
+        <div class="w-full flex flex-col justify-center items-center my-3">
+            <span class="">센서 구성</span>
+            <ul class="flex flex-col justify-start w-full p-3 text-[0.8rem]">
+                <li v-for="(sensorName, index) in sensorList" :key="index" class="flex items-center my-3">
+                    <svg class="mr-2" width="5" height="5" viewBox="0 0 5 5" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="3.6665" cy="3" r="3" fill="#111827" />
+                    </svg>
+
+                    <span>{{ sensorName }}</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    title: String,
+    imageSrc: {
+        type: String,
+        default: "/assets/image/crusher.png"
+    },
+    sensorList: {
+        type: Array,
+        default: ['전력량계', '메인모터(Crusher Speed)', '서브모터(Feeder Speed)']
+    },
+    onClick: {
+        type: Function,
+        required: false
+    },
+    isSelected: {
+        type: Boolean,
+        default: false
+    },
+    color: {
+        type: String,
+        default: '#11B886'
+    }
+
+})
+
+</script>
--- src/pages/User/Reservation/Reservation.vue
+++ src/pages/User/Reservation/Reservation.vue
@@ -1,282 +1,85 @@
 <template>
 
     <div class="mr-[20px]" :style="`margin-left:${leftSideBarWidth}`">
-        <TitleBox width="100%" text="IOT 스마트 생태공장 통합 대시보드" />
+        <TitleBox width="100%" title="한국산업기술시험원 재자원화 실증장비 운영관리플랫폼" :description="new Date()" />
 
-        <div class="flex">
-            <div class="w-1/2 h-full ">
-                <div class="w-full flex  border-solid border-2 
-                border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 pt-3 ">
-
-                    <div class="w-1/2">
-                        <div class="flex items-center mb-2 h-[60px]">
-                            <img class="mr-2" src="/assets/icon/energy_mark.svg" />
-                            <span class="pt-2 font-semibold">종합 에너지 현황</span>
-                        </div>
-
-                        <div>
-
-                            <div class="mb-4">
-                                <div class="text-gray-500">에너지 사용량</div>
-                                <div class="flex justify-between">
-                                    <span class="text-black text-[1.4rem] mr-2 font-semibold">123,123 khw</span>
-                                    <div class="flex items-center mt-3">
-                                        <span>전일대비</span>
-                                        <div
-                                            class="ml-2 bg-[#FDEDF1] text-[#EF4770] text-[12px] font-bold rounded-[16px] px-2 py-1">
-                                            -2.65%</div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="py-2">
-                                <div>
-                                    <span class="">재사용 에너지로 인한 저감 비용</span>
-                                    <div class="flex items-center">
-                                        <input class="pl-[1rem] py-3 rounded-md border-[2px]" type="text"
-                                            value="123,123,123" />
-                                        <button
-                                            class="bg-primary text-white w-[80px] px-2 py-2 rounded-md relative right-[90px] whitespace-nowrap">상세보기</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <main class="px-[15%] py-[20px]">
+            <TitleBox width="100%" title="장비 예약" />
 
 
-                    </div>
-                    <div class="w-1/2">
-                        <div class="flex justify-center items-center w-full h-[60px]">
-                            <span class="mr-2 text-gray-500 text-[0.9rem]">에너지 발전량</span>
-                            <span class="font-bold text-[1.2rem]">123,123 kwh</span>
-                        </div>
-                        <div class="flex justify-center relative bottom-[25px]">
-                            <EnergyDevelopmentChart />
-                        </div>
-
-                    </div>
-
-
-
+            <div class="text-[0.9rem] font-bold flex justify-center mt-[50px] mb-[20px]">장비 선택</div>
+            <div class="xl:flex">
+                <div class="w-full flex justify-between flex-wrap">
+                    <ImageCard title="파쇄기" imageSrc="/assets/image/crusher.png" color="#E5E7EB"
+                        :sensorList="['전력량계', '메인모터(Crusher Speed)', '서브모터(Feeder Speed)']" />
+                    <ImageCard title="분쇄기" imageSrc="/assets/image/grinder.png" color="#E5E7EB"
+                        :sensorList="['전력량계', '메인모터(Pin Mill Speed)', '서브모터(Feeder Speed)']" />
+                    <ImageCard title="로타리 킬른" imageSrc="/assets/image/rotary_kiln.png" color="#E5E7EB"
+                        :sensorList="['전력량계', 'TCK 온도계']" />
+                    <ImageCard title="습식추출장비" imageSrc="/assets/image/wet_extractor.png" color="#E5E7EB"
+                        :sensorList="['전력량계', '튜브연동펌프', 'p H 측정기']" />
                 </div>
-                <div class="flex">
-
-                    <div class="w-1/2 flex flex-col border-solid border-2 
-                border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 py-3 px-3 ">
-                        <div class="flex justify-between w-full mb-2">
-                            <div class="flex items-center">
-                                <img class="w-[29px] h-[29px]" src="/assets/icon/water.svg" />
-                                <span class="mt-2 ml-2">수질 현황</span>
-                            </div>
-                            <button>
-                                <img src="/assets/icon/more.svg" />
-                            </button>
-                        </div>
-                        <span class="text-gray-500 text-[0.9rem]">
-                            평균수치: 123ppm
-                        </span>
-                        <div class="flex ml-auto items-center mt-3">
-                            <span class="text-gray-500 text-[0.9rem] mr-3">수질상태</span>
-                            <div class="bg-[#E7F8F3] text-black text-[0.8rem] font-bold rounded-[16px] px-3 py-1">
-                                2등급
-                            </div>
-                        </div>
-                        <div class="bg-primarySoft py-[20px] my-2 mr-3 flex rounded-md ">
-                            <span class="font-bold ml-[30px]">0.9</span>
-                            <span class="text-gray-500 ml-auto relative top-[15px] mr-2">BOD (mg/L)</span>
-                        </div>
-                        <div class="bg-primarySoft py-[20px] my-2 mr-3 flex rounded-md">
-                            <span class="font-bold ml-[30px]">4.2</span>
-                            <span class="text-gray-500 ml-auto relative top-[15px] mr-2">COD (mg/L)</span>
-                        </div>
-                        <div class="bg-primarySoft py-[20px] my-2 mr-3 flex rounded-md">
-                            <span class="font-bold ml-[30px]">2.8</span>
-                            <span class="text-gray-500 ml-auto relative top-[15px] mr-2">DO (mg/L)</span>
-                        </div>
-                    </div>
-
-                    <div class="w-1/2 flex flex-col border-solid border-2 
-                border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 py-3 px-3 ">
-                        <div class="flex justify-between w-full mb-2">
-                            <div class="flex items-center">
-                                <img class="w-[29px] h-[29px]" src="/assets/icon/waste.svg" />
-                                <span class="mt-2 ml-2">폐기물 배출 현황</span>
-                            </div>
-                            <button>
-                                <img src="/assets/icon/more.svg" />
-                            </button>
-                        </div>
-                        <span class="text-gray-500 text-[0.9rem]">
-                            총 배출량: 123,123 T
-                        </span>
-                        <div class="flex ml-auto items-center mt-3">
-                            <span class="text-gray-500 text-[0.9rem] mr-3">전일대비</span>
-                            <div
-                                class="ml-2 bg-[#FDEDF1] text-[#EF4770] text-[12px] font-bold rounded-[16px] px-2 py-1">
-                                +2.65%</div>
-                        </div>
-                        <div class="flex flex-col justify-center h-full">
-                            <div class="bg-[#FFF9E6] py-[20px] flex flex-col my-2 ml-3 rounded-md ">
-                                <span class="font-bold ml-[30px] mb-2">2,160.36</span>
-                                <span class="text-gray-500 ml-[30px] text-[0.9rem]">배출 저감량</span>
-                            </div>
-                            <div class="bg-[#DBF4EB] py-[20px] flex flex-col my-2 ml-3 rounded-md ">
-                                <span class="font-bold ml-[30px] mb-2">850.65</span>
-                                <span class="text-gray-500 ml-[30px] text-[0.9rem]">재활용량</span>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                </div>
-                <div class="w-2/3 flex flex-col border-solid border-2 
-            border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 py-3 px-3 ">
-                    <div class="flex justify-between items-center">
-                        <div class="flex items-center">
-                            <img class="mr-2" src="/assets/icon/nature.svg" />
-                            <span>금일 대기농도</span>
-                        </div>
-                        <div class="flex items-center">
-                            <img class="mr-2 w-[50px]" src="/assets/icon/warning_circle.svg" />
-                            <span class="text-[0.7rem] font-bold">위험 <br /> (81)</span>
-                        </div>
-                    </div>
-                    <div class="flex justify-center text-[0.8rem] mb-[10%] shadow-lg">
-                        <div class="flex flex-col items-center mr-2">
-
-                            <span>정상</span>
-                            <span class="mb-2">
-                                (0~25)</span>
-                            <ProgressBar class="mb-2" barWidth="70" :percentage="100" barColor="primary"
-                                backgroundColor="primary" />
-                            <img class="w-[1.5rem]" src="/assets/icon/fi-rr-smile.svg" />
-                        </div>
-                        <div class="flex flex-col items-center mr-2">
-
-                            <span>주의</span>
-                            <span class="mb-2">
-                                (26~50)</span>
-                            <ProgressBar class="mb-2" barWidth="70" :percentage="100" barColor="#11B886"
-                                backgroundColor="#11B886" />
-                            <img class="w-[1.5rem]" src="/assets/icon/fi-rr-meh.svg" />
-                        </div>
-                        <div class="flex flex-col items-center mr-2">
-
-                            <span>경계</span>
-                            <span class="mb-2">
-                                (51~75)</span>
-                            <ProgressBar class="mb-2" barWidth="70" :percentage="100" barColor="#FEBF06"
-                                backgroundColor="#FEBF06" />
-                            <img class="w-[1.5rem]" src="/assets/icon/fi-rr-sad.svg" />
-                        </div>
-                        <div class="flex flex-col items-center mr-2">
-
-                            <span>위험</span>
-                            <span class="mb-2">
-                                (76~100)</span>
-                            <ProgressBar class="mb-2" barWidth="70" :percentage="100" barColor="#EE5379"
-                                backgroundColor="#EE5379" />
-                            <img class="w-[2rem]" src="/assets/icon/warning_circle.svg" />
-                        </div>
-                    </div>
-                </div>
-
             </div>
-            <div class="w-1/2">
-                <div class="flex">
-                    <div class="w-1/2 flex flex-col border-solid border-2 
-            border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 py-4 px-4 ">
-                        <div class="flex items-center mb-2">
-                            <img class="mr-2" src="/assets/icon/factory.svg" />
-                            <span class="font-semibold">금일 온실가스 배출</span>
 
-                        </div>
-
-                        <div class="mb-4">
-
-                            <div class="flex justify-between">
-                                <div class="flex items-end">
-                                    <span class="text-black text-[1.4rem] mr-2 font-semibold">123,123 </span>
-                                    <span>tCO2eq</span>
-                                </div>
-
-                            </div>
-                            <div class="flex items-center justify-end py-3 w-full">
-                                <span class='whitespace-nowrap'>전일대비</span>
-                                <div
-                                    class="ml-2 bg-[#FDEDF1] text-[#EF4770] text-[12px] font-bold rounded-[16px] px-2 py-1">
-                                    +2.65%</div>
-                            </div>
-                        </div>
-                        <div class="flex justify-center w-full h-full">
-                            <GreenGasDayChart />
-                        </div>
-
+            <div class="flex my-[50px] px-2 text-[0.9rem]">
+                <div class="w-1/2 pr-2">
+                    <div class="font-bold">예약시작일</div>
+                    <div class="flex justify-center items-center">
+                        시작 일시를 선택하여 주세요 ->
                     </div>
-                    <div class="w-1/2 flex flex-col border-solid border-2 
-            border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 py-3 px-3 ">
-
-
-                        <div class="w-full text-center py-2 font-semibold">설비 운영현황</div>
-
-                        <div class="flex justify-center">
-                            <FacilityOperationChart />
-                        </div>
-
-                        <div class="flex flex-col h-full mx-3">
-                            <div class="flex justify-between items-center mb-4">
-                                <div class="flex items-center">
-                                    <img class="mr-2" src="/assets/icon/sample_1.svg" />
-                                    <span>운영중</span>
-                                </div>
-                                <div class="flex flex-col items-end">
-                                    <span class="font-bold">210대</span>
-                                    <span class="text-[#11B886] text-[14px]">+10 대</span>
-                                </div>
-                            </div>
-                            <div class="flex justify-between items-center mb-4">
-                                <div class="flex items-center">
-                                    <img class="mr-2" src="/assets/icon/sample_2.svg" />
-                                    <span>운영중지</span>
-                                </div>
-
-                                <div class="flex flex-col items-end">
-                                    <span class="font-bold">11</span>
-                                    <span class="text-[#EF4770]">-2 대</span>
-                                </div>
-                            </div>
-                            <div class="flex justify-between items-center">
-                                <div class="flex items-center">
-                                    <img class="mr-2" src="/assets/icon/sample_3.svg" />
-                                    <span>고장</span>
-                                </div>
-
-
-                                <div class="flex flex-col items-end">
-                                    <span class="font-bold">10 대</span>
-                                    <span class="text-[#11B886] text-[14px]">+3 대</span>
-                                </div>
-                            </div>
-                        </div>
-
+                </div>
+                <div class="w-1/2 pr-2">
+                    <div class="font-bold">예약종료일</div>
+                    <div class="flex justify-center items-center">
+                        시작 일시를 선택하여 주세요 ->
                     </div>
+                </div>
+            </div>
 
+            <div class="flex my-[50px] px-2 text-[0.9rem] font-bold">
+                <div class="w-1/2 pr-3">
+                    <div class="mb-2">예약자명</div>
+                    <div class="flex justify-center items-center border-[1px] border-gray rounded-md py-2">
+                        이이수
+                    </div>
+                </div>
+                <div class="w-1/3 pl-3">
+                    <div class=" mb-2">직급</div>
+                    <div class="flex justify-center items-center border-[1px] border-gray rounded-md py-2">
+                        사원
+                    </div>
+                </div>
+            </div>
 
-
+            <div class="flex my-[50px] px-2 text-[0.9rem]">
+                <div class="w-full pr-3">
+                    <div class="mb-2 font-bold">장비이용관련 정보</div>
+                    <input class="w-full flex items-center border-[1px] border-gray rounded-md px-3 py-2"
+                        placeholder="About" />
 
                 </div>
+            </div>
 
-                <div class="w-full flex flex-col border-solid border-2 
-        border-gray-200 px-[20px] cursor-pointer rounded-[16px] hover:bg-gray-200 py-3 px-3 ">
-                    <img class="max-w-[600px] max-h-[600px]" src="/assets/image/factory_sample.png" />
+
+            <div class="flex my-[50px] px-2 text-[0.9rem]">
+                <div class="w-full pr-3">
+                    <div class="mb-2 font-bold">기타사항</div>
+                    <input class="w-full h-[100px] flex border-[1px] border-gray rounded-md px-3 py-2"
+                        placeholder="About" />
                 </div>
-
-
             </div>
 
 
 
-        </div>
+
+
+
+        </main>
+        <footer class="flex justify-center text-[0.9rem] font-bold pb-[50px]">
+            <button class="text-white bg-[#6950E8] mx-3 py-2 w-[180px] rounded-xl">예약하기</button>
+            <button class="text-white bg-[#D1D5DB] mx-3 py-2 w-[180px] rounded-xl">초기화</button>
+
+        </footer>
 
     </div>
 
@@ -303,6 +106,8 @@ import GreenGasDayChart from "./charts/GreenGasDayChart.vue"
 import FacilityOperationChart from './charts/FacilityOperationChart.vue'
 import KanbanBoard from "@/components/KanbanBoard.vue";
 import ProgressBar from "@/components/ProgressBar.vue"
+
+import ImageCard from "./ImageCard.vue"
 
 interface menu {
     menuTitle: string;
@@ -357,7 +162,12 @@ export default {
 
         };
     },
-    components: { Spacer, TitleBox, VViewer, Layout, KanbanBoard, ProcessEnergyUsageChart, EnergyDevelopmentChart, ProgressBar, GreenGasDayChart, FacilityOperationChart }
+    components: {
+        Spacer, TitleBox, VViewer, Layout, KanbanBoard,
+        ProcessEnergyUsageChart, EnergyDevelopmentChart, ProgressBar,
+        GreenGasDayChart, FacilityOperationChart,
+        ImageCard
+    }
 
 };
 </script>
