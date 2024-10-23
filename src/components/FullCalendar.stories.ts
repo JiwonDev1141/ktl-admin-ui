2f0c5843c8b812d09d7637a312186555346d6f94
Jiwon <awesomedev3@rockwonitglobal.com>
2024-10-24 03:35:05 +0900

    feat: fullcalendar 모듈 기본 설정

---
+++ src/components/FullCalendar.stories.ts
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import { ref } from 'vue';
+
+import FullCalendar from './FullCalendar.vue';
+
+import sourceCode from './FullCalendar.vue?raw';
+
+import CodeMirror from './common/CodeMirror.vue';
+
+import dayjs from 'dayjs'
+
+/**
+ * 타입 선언의 영역입니다.
+ */
+const meta: Meta<typeof FullCalendar> = {
+  component: FullCalendar,
+};
+
+export default meta;
+type Story = StoryObj<typeof FullCalendar>;
+
+export const Primary: Story = {
+  render: (args) => ({
+    components: { FullCalendar, CodeMirror },
+    setup() {
+      const codeMirrorRef = ref();
+
+      const hideCodeMirror = (e) => {
+        if( e.target.nextSibling.style.display != 'none') {
+            e.target.nextSibling.style.display = 'none'
+        }
+        else {
+            e.target.nextSibling.style.display = 'block'
+        }
+    };
+   
+
+      return { args, sourceCode, codeMirrorRef, hideCodeMirror };
+    },
+    template: `
+    <div class='flex justify-center absolute right-0 w-1/2 h-full pr-2'>
+    <em class='cursor-pointer' @click='hideCodeMirror'>Code Preview</em>
+    <CodeMirror ref='codeMirrorRef' :sourceCode=sourceCode />
+    </div>
+    <FullCalendar v-bind="args">{{ args.default }}</FullCalendar>
+    `,
+  }),
+  args: {
+    events: [
+        {
+            title: '',
+            date: dayjs().format(),
+            color: 'black'
+        }
+    ]
+  },
+};
---
+++ src/components/FullCalendar.vue
@@ -0,0 +1,30 @@
+<template>
+    <FullCalendar :options="calendarOptions" />
+</template>
+
+<script setup lang="ts">
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import momentPlugin from '@fullcalendar/moment'
+
+interface EventType {
+    title: string;
+    date: string;
+    color?: string;
+}
+
+const props = defineProps({
+    events: Array<EventType>
+})
+
+const calendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin, momentPlugin],
+    initialView: 'dayGridMonth',
+    events: props.events
+}
+
+
+
+
+</script>
